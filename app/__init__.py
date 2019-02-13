6a052d914ad927452a90146d637b8e9c2554f8f0
arguement <ultamatejordan@gmail.com>
2019-02-13 00:39:21 -0500

    finish init.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,12 @@
 from flask import Flask
+from flask_mail import Mail
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'enter some random passphrase'
+app.config['MAIL_SERVER'] = 'smtp.mailtrap.io'
+app.config['MAIL_PORT'] = '465'
+app.config['MAIL_USERNAME'] = 'cda0c02f8133ad'
+app.config['MAIL_PASSWORD'] = '1018564036c41d'
+
+mail = Mail(app)
 from app import views
